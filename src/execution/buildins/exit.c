fb731a8b5da0fea0342888b5d4414f5bf3fe3eef
Younes Kerdel <ykerdel@2-H-2.42heilbronn.de>
2023-08-29 19:52:22 +0200

    rdy

--- src/execution/buildins/exit.c
+++ src/execution/buildins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ykerdel <ykerdel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 19:29:30 by ykerdel           #+#    #+#             */
-/*   Updated: 2023/08/29 19:49:12 by ykerdel          ###   ########.fr       */
+/*   Updated: 2023/08/29 19:51:35 by ykerdel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	bd_exit(t_exe *exe, t_data *g_data)
 	{
 		if (!ft_isdigit(exe->cmd[1][i]))
 		{
-			ft_printf(RED"minishell: exit: %s: numeric argument required\n"CYAN, exe->cmd[1]);
+			ft_printf(RED"minishell: exit: %s: numeric argument required\n"CYAN,
+				exe->cmd[1]);
 			g_exit_status = 255;
 			exit(g_exit_status);
 		}
